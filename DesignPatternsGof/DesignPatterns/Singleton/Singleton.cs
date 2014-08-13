ba33604af6bfc52dbe0c7b1224049a994d090c58
laurentgrangeau <laurent.grangeau@gmail.com>
2014-08-13 11:21:41 +0200

    Initial commit and singleton pattern

---
+++ DesignPatternsGof/DesignPatterns/Singleton/Singleton.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Singleton
+{
+    public class Singleton
+    {
+        public static readonly Singleton Instance = new Singleton();
+
+        private Singleton()
+        {
+
+        }
+    }
+}
